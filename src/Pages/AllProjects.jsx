b49a148d9d75e3192c97df8eb169d549b3aa0b47
Makinde Mayowa <mayowamakinde23@gmail.com>
2024-01-06 19:50:15 +0100

    Update AllProjects.jsx

--- src/Pages/AllProjects.jsx
+++ src/Pages/AllProjects.jsx
@@ -12,13 +12,14 @@ export default function AllProjects() {
   const [ loading, setLoading ] =  useState(true);
     
     useEffect(()=>{
-        async function unsub(){
-            const call = await getProjects(setProjects);
-        } 
-        return ()=>{
-            unsub();
-            // setLoading(false);
-        }
+        // async function unsub(){
+            // const call = await getProjects(setProjects);
+            const call = getProjects(setProjects);
+        // } 
+        // return ()=>{
+        //     unsub();
+        //     // setLoading(false);
+        // }
     },[])
     console.log(projects);
 
