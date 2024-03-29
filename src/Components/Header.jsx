d4d19dd91066aad449fe4578062e2a7b042b9797
Makinde Mayowa <mayowamakinde23@gmail.com>
2024-03-29 02:58:54 +0100

    ghghg

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,11 +7,11 @@ export default function Header({bg}) {
     const [ menu, setMenu ] = useState(false);
     const { isOpen, onToggle } = useDisclosure();
     const links=[
-        {id:0,label:'Home',href:'/'},
-        {id:1,label:'About me',href:'/about'},
-        {id:2,label:'My skills',href:'/skills'},
-        {id:3,label:'Portfolio',href:'/portfolio'},
-        {id:4,label:'Contacts',href:'/#contact'},
+        {label:'Home',href:'/'},
+        {label:'About me',href:'/about'},
+        {label:'My skills',href:'/skills'},
+        {label:'Portfolio',href:'/portfolio'},
+        {label:'Contacts',href:'/#contact'},
     ]
     const handle = () =>{
         setMenu(!menu)
@@ -23,14 +23,14 @@ export default function Header({bg}) {
             <Link to='/'><Text fontWeight='bold' fontSize='20px'>Scared crow</Text></Link>
             <SlideFade in={isOpen} display={{sm:'none',base:'flex'}}>
                 <Flex align={{sm:'end',base:'center'}} w={{sm:'40%',base:'100%'}} bg={{sm:'transparent',base:'rgb(0,0,0)'}} pos={{sm:'initial',base:'fixed'}} h='100%' top='0' left='0' zIndex={{sm:'10',base:'30'}} justify={{sm:'space-between',base:'space-around'}} flexDir={{sm:'row',base:'column'}} display={{sm:'flex', base:menu?'flex':'none'}}>
-                    {links.map(link=>(
-                        <Link key={link.id} color='white' onClick={()=>setMenu(false)} to={link.href} _hover={{borderBottom:'1px solid orange'}}>{link.label}</Link>
+                    {links.map((link,id)=>(
+                        <Link key={id} color='white' onClick={()=>setMenu(false)} to={link.href} _hover={{borderBottom:'1px solid orange'}}>{link.label}</Link>
                     ))}
                 </Flex>
             </SlideFade>
             <Flex align={{sm:'end',base:'center'}} w={{sm:'40%',base:'100%'}} bg={{sm:'transparent',base:'rgb(0,0,0)'}} pos={{sm:'initial',base:'fixed'}} h='100%' top='0' left='0' zIndex={{sm:'10',base:'30'}} justify={{sm:'space-between',base:'space-around'}} flexDir={{sm:'row',base:'column'}} display={{sm:'flex', base:'none'}}>
-                    {links.map(link=>(
-                        <Link key={link.id} color='white' onClick={()=>setMenu(false)} to={link.href} _hover={{borderBottom:'1px solid orange'}}>{link.label}</Link>
+                    {links.map((link,id)=>(
+                        <Link key={id} color='white' onClick={()=>setMenu(false)} to={link.href} _hover={{borderBottom:'1px solid orange'}}>{link.label}</Link>
                     ))}
             </Flex>
             <IconButton color='white' fontSize='24px' zIndex='50' icon={<RiMenu3Fill/>} onClick={handle} variant='ghost' display={{sm:'none',base:'flex'}}/>
